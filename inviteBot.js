6b141da2103935338aa5e3abc8c91e3cb5a2859a
Dustin Yost <dustin.yost.t@gmail.com>
2019-08-29 12:05:12 -0400

    tweak debug

--- inviteBot.js
+++ inviteBot.js
@@ -1,5 +1,5 @@
 const open = require('open');
-console.log(process);
+console.log(process.cwd());
 const Secrets = require(process.argv[2]);
 // opens the url in the default browser 
 open(`https://discordapp.com/oauth2/authorize?client_id=${Secrets.clientId}&scope=bot`);
