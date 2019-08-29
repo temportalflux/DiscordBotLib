fd1b5b68f9f1461134a56d4e6b7488a173eecb3c
Dustin Yost <dustin.yost.t@gmail.com>
2019-08-29 11:59:57 -0400

    Update helper scripts

--- inviteBot.js
+++ inviteBot.js
@@ -1,5 +1,5 @@
 const open = require('open');
-const Secrets = require('./secrets.json');
+const Secrets = require(process.argv[2]);
 
 // opens the url in the default browser 
 open(`https://discordapp.com/oauth2/authorize?client_id=${Secrets.clientId}&scope=bot`);
