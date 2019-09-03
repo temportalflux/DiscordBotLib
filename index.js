284efe101f5de0fd10001399d9d8505b3e928e5c
temportalflux <dustin.yost.t@gmail.com>
2019-09-03 19:35:29 -0400

    Add command listener, discordbot, and database to api

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 module.exports = {
 	Application: require('./src/Application.js'),
+	CommandListener: require('./src/CommandListener.js'),
+	DiscordBot: require('./src/DiscordBot.js'),
+	Database: require('./src/Database.js'),
+
 	Utils: require('./src/utils/index.js'),
 	TemplateCommands: require('./src/commands/index.js'),
 	Service: {
